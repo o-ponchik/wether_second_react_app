6a25850b31d4ca47b5b871bcf3caa3897c3482b7
o-ponchik <105226321+o-ponchik@users.noreply.github.com>
2022-09-19 22:26:10 +0300

    added_link_to_github_and_portfolio

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <p>
+        This project was created by{" "}
+        <a
+          href="https://lighthearted-pony-a59add.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mariia Matviian
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/o-ponchik/wether_second_react_app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub .
+      </p>
     </div>
   );
 }
