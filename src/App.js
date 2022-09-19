770d570623038753e32b43714a2bdf228b94db15
o-ponchik <105226321+o-ponchik@users.noreply.github.com>
2022-09-19 22:57:40 +0300

    added_weather_js_component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <Weather />
         <footer>
           This project was created by{" "}
           <a
