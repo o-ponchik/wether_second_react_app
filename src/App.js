105837f0237b1130519989b72bac1aaced3ed452
o-ponchik <105226321+o-ponchik@users.noreply.github.com>
2022-09-19 22:49:16 +0300

    added_bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <p>
-        This project was created by{" "}
-        <a
-          href="https://lighthearted-pony-a59add.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Mariia Matviian
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/o-ponchik/wether_second_react_app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced
-        </a>{" "}
-        on GitHub .
-      </p>
+      <div className="container">
+        <footer>
+          This project was created by{" "}
+          <a
+            href="https://lighthearted-pony-a59add.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mariia Matviian
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/o-ponchik/wether_second_react_app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+          on GitHub .
+        </footer>
+      </div>
     </div>
   );
 }
